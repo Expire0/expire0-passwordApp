03f9f2f30d79050c69a5e9c761ed7aab0f5ab7ac
Will Woodford <will62185@gmail.com>
2019-10-15 22:48:50 -0400

    Add exit option in getpass.py (#12)

--- getpass.py
+++ getpass.py
@@ -10,18 +10,19 @@ newpass = password.generate_pass(int(pwdlen))
 print(f"Your new password is {newpass}")
 
 print("""
-      Expire0 Pytool .Select a option below
-      1. login lookup
+      Expire0 Pytool .Select a option below:
+      1. Login lookup
       2. Insert new entry 
       3. Delete a entry
       4. Backup data to a sql file 
       5. Modify a entry
+      6. Exit
      """)
 
-selection = input("Select a menu option:")
+selection = input("Select a menu option: ")
 selection1 = int(selection)
 
-if selection1 <= 5:
+if selection1 <= 6:
     if selection1 == 1:
         classm.exconnect()
     if selection1 == 2:
@@ -32,6 +33,8 @@ if selection1 <= 5:
         print(classm.backup())
     if selection1 == 5:
         classm.mod()
+    if selection1 == 6:
+        exit()
 
 else:
     print("Please select a valid option")
